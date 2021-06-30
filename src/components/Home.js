233588f97918d81ce914a85eb5a5a1c128f86c19
Anika Choudhury <anikachoudhury010@gmail.com>
2021-06-30 08:53:28 +0100

    updated introductory statement

--- src/components/Home.js
+++ src/components/Home.js
@@ -17,7 +17,8 @@ function Home() {
                 <p id="line-one">Hi, my name is</p>
                 <h1>Anika Choudhury.</h1>
                 <h2>I build things that live on the web.</h2>
-                <p id="line-four">I am a London-based front-end web developer who specialises in building extraordinary virtual experiences.</p>
+                <p id="line-four">I am a London-based front-end web developer who builds 
+                virtual experiences.</p>
                 <button><a href="mailto:anikachoudhury010@gmail.com">Say Hello</a></button>
             </div>
         </div>
